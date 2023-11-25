ce02f4a2ea1b45d3fdc7e1315d289fc3ead4d04c
Riyabhimani <146011638+Riyabhimani@users.noreply.github.com>
2023-11-25 18:57:00 +0530

    Add files via upload

---
+++ LAB - 9/Uppercase_lowercase.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+   void main(){
+   	char a=97,c=65;
+   	while(a>=97&&a<=122){
+   		printf("%c\n",a);
+   		a++;
+	   }
+	   while(c>=65&&c<=90){
+	   	printf("%c",c);
+	   	c++;
+	   }
+   }
